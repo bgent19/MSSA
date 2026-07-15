f5318f94cbadf8bf6ba0ef46823e01a5c7ed6ec7
Brett Gentile <bgentile19@gmail.com>
2026-07-15 11:22:46 -0400

    Assignment 2.4.1 and 2.4.2 solutions

---
+++ Assignment2.4.1/Program.cs
@@ -0,0 +1,36 @@
+﻿//Write a program in C# Sharp to find the sum of all array elements.
+
+//Test Data :
+
+//Input the number of elements to be stored in the array :3
+
+//Input 3 elements in the array :
+
+//element - 0 : 2
+
+//element - 1 : 5
+
+//element - 2 : 8
+
+//Expected Output :
+
+//Sum of all elements stored in the array is : 15
+
+Console.Write("Input the number of elements to be stored in the array : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Input {n} elements in the array :");
+
+int[] nums = new int[n];
+for (int i = 0; i < nums.Length; i++)
+{
+    Console.Write($"element - {i} : ");
+    nums[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int sum = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    sum += nums[i];
+}
+Console.WriteLine($"Sum of all elements stored in the array is : {sum}");
---
+++ Assignment2.4.2/Program.cs
@@ -0,0 +1,40 @@
+﻿//Write a C# Sharp program to find the largest of three numbers.
+
+//Test Data :
+
+//Input the 1st number :25
+
+//Input the 2nd number :63
+
+//Input the 3rd number :10
+
+//Expected Output :
+
+//The 2nd Number is the greatest among three
+
+Console.WriteLine("Test Data :");
+
+Console.Write("Input the 1st number : ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Input the 2nd number : ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Input the 3rd number : ");
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine("The 1st Number is the greatest among three");
+}
+else if (num2 > num1 && num2 > num3)
+{
+    Console.WriteLine("The 2nd Number is the greatest among three");
+}
+else if (num3 > num1 && num3 > num2)
+{
+    Console.WriteLine("The 3rd Number is the greatest among three");
+}
+else
+{
+    Console.WriteLine("There is no single greatest number.");
+}
+
