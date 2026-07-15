23fbcfbeaa34613ae3dafd3d0bc0d6d0bb461cea
Brett Gentile <bgentile19@gmail.com>
2026-07-15 13:44:07 -0400

    Fun mathy way to solve

--- Assignment2.4.3/Program.cs
+++ Assignment2.4.3/Program.cs
@@ -16,15 +16,29 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input the value for the Y coordinate: ");
 int y = Convert.ToInt32(Console.ReadLine());
 
-string location = (x, y) switch
-{
-    (> 0, > 0) => "First quadrant",
-    (< 0, > 0) => "Second quadrant",
-    (< 0, < 0) => "Third quadrant",
-    (> 0, < 0) => "Fourth quadrant",
-    (0, not 0) => "Y-axis",
-    (not 0, 0) => "X-axis",
-    (0, 0) => "origin"
-};
+//string location = (x, y) switch
+//{
+//    (> 0, > 0) => "First quadrant",
+//    (< 0, > 0) => "Second quadrant",
+//    (< 0, < 0) => "Third quadrant",
+//    (> 0, < 0) => "Fourth quadrant",
+//    (0, not 0) => "Y-axis",
+//    (not 0, 0) => "X-axis",
+//    (0, 0) => "origin"
+//};
 
-Console.WriteLine($"The coordinate point({x}, {y}) lies in the {location}.");
+if (x != 0 && y != 0)
+{
+    // Convert (-PI to PI) into (0 to 2PI) range
+    double angle = (Math.Atan2(y, x) + 2 * Math.PI) % (2 * Math.PI);
+
+    // Divide by PI/2 (90 degrees) and round down to get 0, 1, 2, or 3, then add 1
+    int location = Convert.ToInt32(Math.Floor(angle / (Math.PI / 2)) + 1);
+
+    Console.WriteLine($"The coordinate point({x}, {y}) lies in Quadrant {location}.");
+}
+else
+{
+    Console.WriteLine("The coordinate point lies on an axis or at the origin.");    
+}
+
