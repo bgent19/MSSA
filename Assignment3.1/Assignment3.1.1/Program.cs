3619e959f9768826c505d134a52ec0e10dea91c4
Brett Gentile <bgentile19@gmail.com>
2026-07-20 11:59:28 -0400

    Assignment 3.1.1 submission

---
+++ Assignment3.1/Assignment3.1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+Console.WriteLine(EvenNumbers(0, 100));
+
+
+static string EvenNumbers(int min, int max)
+{
+    StringBuilder sb = new();
+
+    // find first even number from min
+    if (min == 0)
+        min = 2;
+    else if ((min % 2) != 0)
+        ++min;
+
+    for(int i = min; i < max; i += 2)
+    {
+        sb.AppendFormat("{0} ", i);
+    }
+
+    return sb.ToString();
+}
