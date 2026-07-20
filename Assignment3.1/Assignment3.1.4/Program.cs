d6824beb9b282999d68adf73ec45f4d65ccc14d3
Brett Gentile <bgentile19@gmail.com>
2026-07-20 14:14:49 -0400

    Assignment3.1.4 submission

---
+++ Assignment3.1/Assignment3.1.4/Program.cs
@@ -0,0 +1,29 @@
+﻿Random rand = new();
+
+int[] arr = new int[50];
+
+for(int i = 0; i < arr.Length; ++i)
+{
+    arr[i] = rand.Next(5);
+}
+
+Console.WriteLine(string.Join(" ", arr));
+TransformTwoOnesOnce(arr);
+Console.WriteLine(string.Join(" ", arr));
+
+static void TransformTwoOnesOnce(int[] a)
+{
+    for(int cur = 1; cur < a.Length; ++cur)
+    {
+        int prev = cur - 1;
+
+        if(a[cur] == a[prev] && a[prev] == 1)
+        {
+            a[prev] = 0;
+            a[cur] = 0;
+            break;
+        }
+    }
+}
+
+
