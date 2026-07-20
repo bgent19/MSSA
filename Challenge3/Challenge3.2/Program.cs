64dbdea1da727c890b17174316bc321768f8ff0e
Brett Gentile <bgentile19@gmail.com>
2026-07-20 18:39:27 -0400

    Challenge3.2 submission

---
+++ Challenge3/Challenge3.2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter text (terminated by ENTER): ");
+string text = Console.ReadLine();
+
+int sum = 0;
+foreach (var c in text.Where(c => Char.IsAsciiDigit(c)))
+{
+    sum += c - '0'; // Has to be more efficient then cast char to double to int
+                    // same logic as "" vs string.Empty
+                    // Trade clairty in intent for efficient operations
+}
+
+Console.WriteLine($"The sum of the digits in that text was {sum}");
