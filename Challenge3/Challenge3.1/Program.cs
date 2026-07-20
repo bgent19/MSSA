ea0df5bb7b996d0b59011967724ebaf90e9e5865
Brett Gentile <bgentile19@gmail.com>
2026-07-20 18:14:12 -0400

    Challenge3.1 Submission

---
+++ Challenge3/Challenge3.1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Type a word or phrase (terminated by ENTER): ");
+string text = Console.ReadLine();
+
+Console.WriteLine($"Was that a palindrome? {IsPalindrome(text)}");
+
+
+static bool IsPalindrome(string s)
+{
+
+    // clean text
+    string r = new string(s.Where(c => !char.IsWhiteSpace(c) && !char.IsPunctuation(c)).ToArray());
+    r = r.ToLower();
+
+    int len = r.Length;
+    int half = len / 2;
+
+    for(int i = 0; i < half; ++i)
+    {
+        if (r[i] != r[len - 1 - i])
+        {
+            return false;
+        }
+
+    }
+
+    return true;
+}
