9af04a239988fce9486ea72eefc4b27052623ce6
Brett Gentile <bgentile19@gmail.com>
2026-07-20 13:45:09 -0400

    Assignment 3.1.3 submission

---
+++ Assignment3.1/Assignment3.1.3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Type to your heart's content and press ENTER when you are finished: ");
+string? text = Console.ReadLine();
+
+Console.WriteLine($"Your text contained {NumSpaces(text)} spaces!");
+
+static int NumSpaces(string? s)
+{
+    int count = 0;
+
+    if (s is null)
+        return count;
+
+    foreach (char c in s)
+    {
+        if(c == ' ')
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
