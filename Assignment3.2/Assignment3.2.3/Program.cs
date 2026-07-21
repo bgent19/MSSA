16c622eb75f93c56c513990b41004b83ea48977d
Brett Gentile <bgentile19@gmail.com>
2026-07-21 13:54:31 -0400

    Assignment3.2.3 submission

---
+++ Assignment3.2/Assignment3.2.3/Program.cs
@@ -0,0 +1,45 @@
+﻿Circle one = new() { Area = 2 };
+Circle two = new Circle { Area = 3 };
+
+Console.WriteLine($"Circle one: radius = {one.Radius:F2} units");
+Console.WriteLine($"              area = {one.Area:F2} units");
+
+Console.WriteLine($"Circle two: radius = {two.Radius:F2} units");
+Console.WriteLine($"              area = {two.Area:F2} units");
+
+Circle sum = one + two;
+Circle diff = two - one;
+
+Console.WriteLine($"Circle sum: radius = {sum.Radius:F2} units");
+Console.WriteLine($"              area = {sum.Area:F2} units");
+
+Console.WriteLine($"Circle dif: radius = {diff.Radius:F2} units");
+Console.WriteLine($"              area = {diff.Area:F2} units");
+
+public class Circle
+{
+    public double Radius { get; set; }
+
+    public double Area {
+        get
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        set
+        {
+            Radius = Math.Sqrt(value / Math.PI);
+        }
+    }
+
+    public static Circle operator +(Circle c1, Circle c2)
+    {
+        return new Circle() { Area = c1.Area + c2.Area };
+    }
+
+    public static Circle operator -(Circle c1, Circle c2)
+    {
+        return new Circle() { Area = c1.Area - c2.Area };
+    }
+
+}
