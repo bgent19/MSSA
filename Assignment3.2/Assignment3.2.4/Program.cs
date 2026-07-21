87730fd7563167c805a3cecbc80aa7da21a33139
Brett Gentile <bgentile19@gmail.com>
2026-07-21 14:05:16 -0400

    Assignment3.2.4 submission

---
+++ Assignment3.2/Assignment3.2.4/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] nums = new int[4];
+
+Console.Write("Enter the First number: ");
+nums[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the Second number: ");
+nums[1] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the Third number: ");
+nums[2] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the Fourth number: ");
+nums[3] = Convert.ToInt32(Console.ReadLine());
+
+int sum;
+double avg;
+
+SumAndAvg(nums, out sum, out avg);
+
+Console.WriteLine($"The average of {string.Join(", ", nums)} is: {avg}");
+Console.WriteLine($"The total is {sum}");
+
+
+static void SumAndAvg(int[] n, out int s, out double a)
+{
+    s = 0;
+    foreach(int i in n)
+    {
+        s += i;
+    }
+
+    a = (double)s / n.Length;
+}
