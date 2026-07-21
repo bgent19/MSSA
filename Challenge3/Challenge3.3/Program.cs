315c5e3adfb97778ad3d237a250e59967c44355b
Brett Gentile <bgentile19@gmail.com>
2026-07-21 17:52:13 -0400

    Challenge3.3 submission

---
+++ Challenge3/Challenge3.3/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int[] nums = [2, 7, 11, 15];
+int target = 9;
+
+int[] result = SumToTarget(nums, target);
+
+Console.WriteLine($"[{string.Join(',',result)}]");
+
+
+
+int[] SumToTarget(int[] array, int target)
+{
+    int[] results = new int[2];
+    Dictionary<int, int> remains = [];
+
+    foreach(var (index, item) in array.Index())
+    {
+        if(remains.ContainsKey(target-item))
+        {
+            results[0] = remains[target - item];
+            results[1] = index;
+            break;
+        }
+        else
+        {
+            remains[item] = index;
+        }
+    }
+
+    return results;
+}
