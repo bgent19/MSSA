f8dc3be4c872d18a3fd7a5f51e10d6665756976d
Brett Gentile <bgentile19@gmail.com>
2026-07-21 12:39:41 -0400

    Assignment 3.2.1 submission

---
+++ Assignment3.2/Assignment3.2.1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Console.Write("What dimension (n): ");
+
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int l = 1;
+
+int[,] matrix = new int[n,n];
+
+for(int i = 0; i < n*n; ++i)
+{
+    Console.Write($"Enter number {i}: ");
+    string num = Console.ReadLine();
+    int numL = num.Length;
+
+    if(numL > l)
+    {
+        l = numL;
+    }
+    matrix[i / n, i % n] = Convert.ToInt32(num);
+}
+Console.WriteLine();
+
+for (int i = 0; i < n; ++i)
+{
+    for (int j = 0; j < n; ++j)
+    {
+        Console.Write("\t|\t" + string.Format("{0," + l + "}", matrix[i, j]));
+    }
+    Console.WriteLine("\t|");
+    Console.WriteLine();
+}
+
+
