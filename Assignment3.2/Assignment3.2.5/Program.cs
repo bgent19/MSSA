7b774e559f08b728de754d5baa1c09b3e4ebb8b5
Brett Gentile <bgentile19@gmail.com>
2026-07-21 17:37:00 -0400

    Assignment3.2.5 submission

---
+++ Assignment3.2/Assignment3.2.5/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Search([1, 5, 3], 5) ➞ ");
+Console.WriteLine(Search([1, 5, 3], 5));
+
+Console.Write("Search([9, 8, 3], 3) ➞ ");
+Console.WriteLine(Search([9, 8, 3], 3));
+
+Console.Write("Search([1, 2, 3], 4) ➞ ");
+Console.WriteLine(Search([1, 2, 3], 4));
+
+
+static int Search(int[] array, int target)
+{
+    int result = -1;
+
+    foreach(var (index, item) in array.Index())
+    {
+        if(item == target)
+        {
+            result = index;
+            break;
+        }
+    }
+
+    return result;
+}
