ba5ab221963fe4c492988fca42f3a8a9aaae4b23
Brett Gentile <bgentile19@gmail.com>
2026-07-27 17:47:20 -0400

    Challenge4.1 Submission

---
+++ Challenge4/Challenge4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿Random rand = new();
+
+for(int i = 0; i < 10; i++)
+{
+    int num = rand.Next();
+    Console.WriteLine($"Does {num} contain 3? {ContainsThree(num)}");
+}
+
+
+
+bool ContainsThree(int n)
+{
+
+    while(n > 0)
+    {
+        if ((n % 10) == 3)
+        {
+            return true;
+        }
+
+        n /= 10;
+    }
+
+    return false;
+}
