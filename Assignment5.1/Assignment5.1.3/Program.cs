d642cc5718cd9b9acd76b29b7e5d865b58d5cb81
Brett Gentile <bgentile19@gmail.com>
2026-08-03 13:24:48 -0400

    Assignment5.1.3 solution

---
+++ Assignment5.1/Assignment5.1.3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(ContainsDuplicate([1, 2, 3, 1])); // true
+Console.WriteLine(ContainsDuplicate([1, 2, 3, 4])); // false
+Console.WriteLine(ContainsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])); // true
+
+static bool ContainsDuplicate(int[] nums)
+{
+    HashSet<int> seen = [];
+
+    foreach(int n in nums)
+    {
+        if(seen.Contains(n))
+        {
+            return true;
+        }
+    
+        seen.Add(n);
+        
+    }
+    return false;
+}
