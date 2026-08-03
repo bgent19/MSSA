a3eada700a64741cc5f3b3ec46ef74af17d25be2
Brett Gentile <bgentile19@gmail.com>
2026-08-03 17:10:52 -0400

    Challenge5.1 Solution

---
+++ Challenge5/Challenge5.1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(MissingNumber([3, 0, 1])); // 2
+Console.WriteLine(MissingNumber([0, 1])); // 2
+Console.WriteLine(MissingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])); // 8
+
+
+static int MissingNumber(int[] nums)
+{
+    int len = nums.Length;
+
+    int sumTotal = len, sumFound = 0;
+
+    for(int i = 0; i < len; i++)
+    {
+        sumTotal += i;
+        sumFound += nums[i];
+    }
+
+    return sumTotal - sumFound;
+}
