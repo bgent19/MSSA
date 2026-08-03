f78f4fd8eeca31f585736298a908467d13967d71
Brett Gentile <bgentile19@gmail.com>
2026-08-03 13:16:33 -0400

    Assignment5.1.2 submission

---
+++ Assignment5.1/Assignment5.1.2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine(AddDigits(1234)); // 10
+
+
+
+static int AddDigits(int n)
+{
+    int result = 0;
+
+    while (n > 0)
+    {
+        result += n % 10;
+
+        n /= 10;
+    }
+
+
+    return result;
+}
