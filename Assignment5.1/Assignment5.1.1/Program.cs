c0013ec70eac3b048fb8113a6e2d9e1f78a6e017
Brett Gentile <bgentile19@gmail.com>
2026-08-03 13:11:21 -0400

    Assignment5.1.1 submission

---
+++ Assignment5.1/Assignment5.1.1/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine(isPalindromic(121)); // true
+Console.WriteLine(isPalindromic(-121)); // false
+Console.WriteLine(isPalindromic(10)); // false
+
+
+static bool isPalindromic(int x)
+{
+    if(x < 0)
+    {
+        return false;
+    }
+
+
+    Stack<int> reverse = new();
+    Queue<int> forward = new();
+
+    while(x > 0)
+    {
+        int n = x % 10;
+        reverse.Push(n);
+        forward.Enqueue(n);
+        x /= 10;
+    }
+
+    while(reverse.Count > 0)
+    {
+        if(reverse.Pop() != forward.Dequeue())
+        {
+            return false;
+        }
+
+    }
+
+    return true;
+}
