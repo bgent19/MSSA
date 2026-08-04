a0f418a1f20a96a9043df3f663003c61e5282ef5
Brett Gentile <bgentile19@gmail.com>
2026-08-04 15:42:35 -0400

    Assignment5.2.3 submission

---
+++ Assignment5.2/Assignment5.2.3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("How many numbers to print: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintNumbersDesc(n);
+
+static void PrintNumbersDesc(int n)
+{
+    // base case
+    if (n == 1)
+    {
+        Console.Write(n + " ");
+        return;
+    }
+
+    // recursive case
+    Console.Write(n + " ");
+    PrintNumbersDesc(n - 1);
+
+}
