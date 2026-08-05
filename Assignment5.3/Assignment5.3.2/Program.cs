337e3ab74877ac6d86cf24d74028b4ee635c2f2a
Brett Gentile <bgentile19@gmail.com>
2026-08-05 16:05:07 -0400

    Assignment5.3.2 solution

---
+++ Assignment5.3/Assignment5.3.2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(ClimbStairs(2)); // 2
+Console.WriteLine(ClimbStairs(3)); // 3
+
+
+static int ClimbStairs(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    int cur = 2, prev = 1;
+
+    for (int i = 2; i < n; i++)
+    {
+        (cur, prev) = (cur + prev, cur);
+    }
+
+    return cur;
+}
