52bb77cca2e002ceff70793ceeca2abc7ded633d
Brett Gentile <bgentile19@gmail.com>
2026-08-05 14:03:18 -0400

    Assignment5.3.1 solution

---
+++ Assignment5.3/Assignment5.3.1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine(CanPlaceFlowers([1, 0, 0, 0, 1], 1)); // true
+Console.WriteLine(CanPlaceFlowers([1, 0, 0, 0, 1], 2)); // false
+
+static bool CanPlaceFlowers(int[] flowerbed, int n)
+{
+
+    int validSpaces = 0;
+    if (flowerbed[0] == 0)
+    {
+        flowerbed[0] = 1;
+        validSpaces++;
+    }
+
+    for (int i = 1; i < flowerbed.Length; i++)
+    {
+        if (flowerbed[i] == 0 && flowerbed[i - 1] == 0)
+        {
+            flowerbed[i] = 1;
+            validSpaces++;
+        }
+
+        if (flowerbed[i] == 1 && flowerbed[i - 1] == 1)
+        {
+            flowerbed[i - 1] = 0;
+            validSpaces--;
+        }
+    }
+
+    return validSpaces >= n;
+}
