59578e8847e1b6ae0858d1bcca686cedfb9fc307
Brett Gentile <bgentile19@gmail.com>
2026-08-06 13:31:11 -0400

    Assignment5.4.1 submission

--- Assignment5.4/Assignment5.4.1/Program.cs
+++ Assignment5.4/Assignment5.4.1/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Input any number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"The digits in the number {num} are {string.Join(' ', Digitize(num))}");
+
+static List<int> Digitize(int n)
+{
+    // base case
+    if(n == 0)
+    {
+        return [];
+    }
+
+    // recursive case
+    List<int> result = Digitize(n / 10);
+    result.Add(n % 10);
+    return result;
+}
