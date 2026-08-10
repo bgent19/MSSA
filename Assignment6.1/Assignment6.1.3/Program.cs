add0ebf6637ad5802c0fd2563a1cd3a345c8f370
Brett Gentile <bgentile19@gmail.com>
2026-08-10 14:13:50 -0400

    Assignment6.1.3 solution

---
+++ Assignment6.1/Assignment6.1.3/Program.cs
@@ -0,0 +1,29 @@
+﻿int[] a = [0, 1, 0, 3, 12];
+Console.WriteLine(ArrayToString(a));
+MoveZeroes(a);
+Console.WriteLine(ArrayToString(a));
+
+int[] b = [0];
+Console.WriteLine(ArrayToString(b));
+MoveZeroes(b);
+Console.WriteLine(ArrayToString(b));
+
+
+void MoveZeroes(int[] arr)
+{
+    int zeroPointer = 0;
+
+    for(int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] != 0 && i > zeroPointer)
+        { 
+            (arr[i], arr[zeroPointer]) = (arr[zeroPointer], arr[i]);
+            while (arr[++zeroPointer] != 0) ; 
+        }
+    }
+}
+
+string ArrayToString(int[] arr)
+{
+    return $"[{string.Join(',', arr)}]";
+}
