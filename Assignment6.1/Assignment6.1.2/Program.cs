b85692e5ae52646a2396a18c8a4a11e7a7037b07
Brett Gentile <bgentile19@gmail.com>
2026-08-10 13:36:49 -0400

    Assignment6.1.2 solution

---
+++ Assignment6.1/Assignment6.1.2/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+LinkedList<House> myList = new();
+
+myList.AddLast(new House() { HouseNumber = 1, StreetName = "Winning Drive", TypeOfHouse = "Facility" });
+myList.AddLast(new House() { HouseNumber = 2, StreetName = "First Loser Lane", TypeOfHouse = "Sad" });
+
+Console.WriteLine(SearchByNumber(myList, 2));
+Console.WriteLine(SearchByNumber(myList, 3));
+
+House? SearchByNumber(LinkedList<House> l, int num)
+{
+    foreach (House h in myList)
+    {
+        if (h.HouseNumber == num)
+        {
+            return h;
+        }
+
+    }
+
+    return null;
+}
+
+class House
+{
+    public int HouseNumber { get; set; }
+    public string? StreetName { get; set; }
+
+    public string? TypeOfHouse { get; set; }
+
+    public override string ToString()
+    {
+        return $"{HouseNumber} {StreetName} ({TypeOfHouse})";
+    }
+}
+
