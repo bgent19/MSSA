eb3e1f6cd7e7514cb765a03a988b6fbb081fcff9
Brett Gentile <bgentile19@gmail.com>
2026-08-11 17:25:23 -0400

    Challenege6.1 submission

---
+++ Challenge6/Chalenge6.1/Program.cs
@@ -0,0 +1,49 @@
+﻿
+int[][] a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+PrintMatrix(a);
+Console.WriteLine();
+Rotate(a);
+PrintMatrix(a);
+Console.WriteLine();
+
+int[][] b = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]];
+PrintMatrix(b);
+Console.WriteLine();
+Rotate(b);
+PrintMatrix(b);
+Console.WriteLine();
+
+static void Rotate(int[][] matrix)
+{
+    // Step 1: Transpose
+    for (int row = 0; row < matrix.Length; row++)
+    {
+        for (int col = 0; col < matrix.Length; col++)
+        {
+            if(row < col)
+            {
+                (matrix[row][col], matrix[col][row]) =
+                (matrix[col][row], matrix[row][col]);
+            }
+        }
+    }
+
+
+    // Step 2: Reverse Cols
+    for (int row = 0; row < matrix.Length; row++)
+    {
+        for (int col = 0; col < matrix.Length / 2; col++)
+        {
+            (matrix[row][col], matrix[row][matrix.Length - 1 - col]) =
+            (matrix[row][matrix.Length - 1 - col], matrix[row][col]);
+        }
+    }
+}
+
+static void PrintMatrix(int[][] matrix)
+{
+    foreach (int[] arr in matrix)
+    {
+        Console.WriteLine(string.Join('\t', arr));
+    }
+}
