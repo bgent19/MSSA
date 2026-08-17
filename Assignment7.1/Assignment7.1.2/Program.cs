7b22d3f7a6b144d916ffe22f911540c6e0e60b21
Brett Gentile <bgentile19@gmail.com>
2026-08-17 14:04:42 -0400

    Assignment7.1.2 solution

---
+++ Assignment7.1/Assignment7.1.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+Console.WriteLine(MergeAlternately("abc", "pqr"));
+Console.WriteLine(MergeAlternately("ab", "pqrs"));
+Console.WriteLine(MergeAlternately("abcd", "pq"));
+
+static string MergeAlternately(string word1, string word2)
+{
+    StringBuilder result = new(word1.Length + word2.Length);
+
+    int i = 0, j = 0;
+
+    while (i < word1.Length && j < word2.Length)
+    {
+        result.Append([word1[i++], word2[j++]]);
+    }
+
+    while (i < word1.Length)
+    {
+        result.Append(word1[i++]);
+    }
+
+    while (j < word2.Length)
+    {
+        result.Append(word2[j++]);
+    }
+
+    return result.ToString();
+}
