617e68193c41a591b13de56f2227e8aea48d524e
Brett Gentile <bgentile19@gmail.com>
2026-08-18 16:20:27 -0400

    Assignment7.2.3 solution

---
+++ Assignment7.2/Assignment7.2.3/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine(IsAnagram("anagram", "nagaram")); // true
+Console.WriteLine(IsAnagram("rat", "car")); // false
+
+bool IsAnagram(string s, string t)
+{
+    Dictionary<char, int> letters = [];
+
+    foreach(char c in s)
+    {
+        if(!letters.ContainsKey(c))
+        {
+            letters[c] = 1;
+        }
+        else
+        {
+            letters[c]++;
+        }
+    }
+
+    foreach (char c in t)
+    {
+        if (letters.ContainsKey(c))
+        {
+            letters[c]--;
+
+            if (letters[c] == 0)
+            {
+                letters.Remove(c);
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    return letters.Count() == 0;
+}
