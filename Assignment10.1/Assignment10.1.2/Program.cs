20ba5eb3d42755f97333e74213ab6c0a3dd2d65d
Brett Gentile <bgentile19@gmail.com>
2026-09-08 16:58:53 -0400

    refactor 10.1.2 to remove redundant lines.

--- Assignment10.1/Assignment10.1.2/Program.cs
+++ Assignment10.1/Assignment10.1.2/Program.cs
@@ -9,17 +9,11 @@ foreach(string answer in results)
 
 
 IList<string> GenerateParenthesis(int n)
-{
-
-    IList<string> results = [];
-
-    results = recursiveGenPar("", 2 * n, 0);
-
-
-
-    return results;
+{ 
+    return recursiveGenPar("", 2 * n, 0);
 }
 
+// Weight = # of '(' minus # of ')'
 IList<string> recursiveGenPar(string soFar, int numLeft, int weight)
 {
     if (numLeft == 0)
@@ -30,14 +24,14 @@ IList<string> recursiveGenPar(string soFar, int numLeft, int weight)
     List<string> results = [];
 
     // add '('
-    if (weight < numLeft)
+    if (weight < numLeft) // if this is false, it means there arent enough spaces left to add the required ')'
     {
         results.AddRange(recursiveGenPar(soFar + '(', numLeft - 1, weight + 1));
     }
 
     // add ')'
 
-    if (weight > 0)
+    if (weight > 0) // If this is false, it means there isn't any remanining '(' to the left of a new ')'
     {
         results.AddRange(recursiveGenPar(soFar + ')', numLeft - 1, weight - 1));
     }
