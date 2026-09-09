23abb3ab18128de900c810d9fc924511610da17e
Brett Gentile <bgentile19@gmail.com>
2026-09-09 13:33:50 -0400

    Assignment 10.2.4 solution

--- Assignment10.2/Assignment10.2.4/Program.cs
+++ Assignment10.2/Assignment10.2.4/Program.cs
@@ -1,2 +1,18 @@
 // Mock data source: list of numbers to filter for values greater than 80.
+
+Random rand = new();
+
 List<int> numbers = new List<int> { 55, 200, 740, 76, 230, 482, 95 };
+
+for(int i = 0; i < 7; i++)
+{
+    numbers.Add(rand.Next(0, 1000));
+}
+
+Console.WriteLine(string.Join(", ", numbers));
+
+var numQuery = from num in numbers
+               where num > 80
+               select num;
+
+Console.WriteLine(string.Join(", ", numQuery));
