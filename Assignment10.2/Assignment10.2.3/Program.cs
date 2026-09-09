8d11f522239cddd1d3203c9dfd995df4d13ac5dd
Brett Gentile <bgentile19@gmail.com>
2026-09-09 13:23:11 -0400

    Assignment10.2.3 solution

--- Assignment10.2/Assignment10.2.3/Program.cs
+++ Assignment10.2/Assignment10.2.3/Program.cs
@@ -14,3 +14,9 @@ List<string> cities = new List<string>
 
 char startCharacter = 'A';
 char endCharacter = 'M';
+
+var cityQuery = from city in cities
+                where (city.First() == startCharacter && city.Last() == endCharacter)
+                select city;
+
+Console.WriteLine(string.Join(", ", cityQuery));
