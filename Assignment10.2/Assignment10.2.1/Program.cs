17c0d9fdca301d7ffa2687343d0420835e164feb
Brett Gentile <bgentile19@gmail.com>
2026-09-09 13:12:58 -0400

    Assignment 10.2.1 solution

--- Assignment10.2/Assignment10.2.1/Program.cs
+++ Assignment10.2/Assignment10.2.1/Program.cs
@@ -1,2 +1,8 @@
 // Mock data source: list of numbers to filter for positives.
 List<int> numbers = new List<int> { 2, -1, 3, -3, 10, -200 };
+
+var numQuery = from num in numbers
+               where (num > 0)
+               select num;
+
+Console.WriteLine(string.Join(", ", numQuery));
