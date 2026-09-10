003ed5b19431a5b7d0894837a9b0dad19fd4afcf
Brett Gentile <bgentile19@gmail.com>
2026-09-10 11:23:28 -0400

    Challenge10.1 solution

---
+++ Challenge10/Challenge10.1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine(FindTheDifference("abcd", "abcde")); // output: e
+Console.WriteLine(FindTheDifference("", "y")); // output: y
+
+char FindTheDifference(string s, string t)
+{
+    Dictionary<char, int> letters = [];
+
+    foreach (char c in s)
+    {
+        letters[c] = letters.GetValueOrDefault(c, 0) + 1;
+    }
+
+    foreach (char c in t)
+    {
+        int val;
+        bool found = letters.TryGetValue(c, out val);
+
+        if(!found || val == 0)
+        {
+            return c;
+        }
+        else
+        {
+            letters[c]--;
+        }
+    }
+
+    return t[0];
+}
