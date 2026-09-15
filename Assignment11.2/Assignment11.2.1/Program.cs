f1bbd45fb3e6b9fc34eec87565060666711c889a
Brett Gentile <bgentile19@gmail.com>
2026-09-15 12:13:27 -0400

    Assignment11.2.1 solution

---
+++ Assignment11.2/Assignment11.2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(MaxProfit([7, 1, 5, 3, 6, 4])); // Expect: 5
+Console.WriteLine(MaxProfit([7, 6, 4, 3, 1])); // Expect: 0
+
+
+int MaxProfit(int[] prices)
+{
+
+    int minI = 0;
+    int maxI = 0;
+    int curMax = 0;
+
+    for (int i = 0; i < prices.Length; i++)
+    {
+        if (prices[i] < prices[minI])
+        {
+            curMax = Math.Max(curMax, prices[maxI] - prices[minI]);
+            minI = maxI = i;
+        }
+        else if (prices[i] > prices[maxI])
+        {
+            maxI = i;
+        }
+    }
+
+    return Math.Max(curMax, prices[maxI] - prices[minI]);
+}
