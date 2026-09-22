58097f7272a0ab54bb750343211af1a32a6e8a16
Brett Gentile <bgentile19@gmail.com>
2026-09-22 12:19:56 -0400

    Assignment12.1.1 solution

---
+++ Assignment12.1/Assignment12.1.1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine(CanConstruct("a", "b")); // False
+Console.WriteLine(CanConstruct("aa", "ab")); // False
+Console.WriteLine(CanConstruct("aa", "aab")); // True
+
+static bool CanConstruct(string ransomNote, string magazine)
+{
+
+    int[] letterFreq = new int[26];
+
+    foreach (char c in magazine)
+    {
+        letterFreq[c - 'a']++;
+    }
+
+    foreach (char c in ransomNote)
+    {
+        letterFreq[c - 'a']--;
+    }
+
+    foreach (int leftOver in letterFreq)
+    {
+        if (leftOver < 0)
+            return false;
+    }
+
+    return true;
+}
