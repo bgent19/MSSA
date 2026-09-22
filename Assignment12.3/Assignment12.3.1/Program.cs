336c174695a782714141db6da6bece7cbe445af5
Brett Gentile <bgentile19@gmail.com>
2026-09-22 13:30:33 -0400

    Assignment12.3.1 solution

---
+++ Assignment12.3/Assignment12.3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine(RemoveDuplicates("abbaca")); // "ca"
+Console.WriteLine(RemoveDuplicates("azxxzy")); // "ay"
+
+static string RemoveDuplicates(string s)
+{
+
+    Stack<char> letters = new();
+
+    foreach (char c in s)
+    {
+        if (letters.TryPeek(out char top))
+        {
+            if (top == c)
+            {
+                letters.Pop();
+                continue;
+            }
+        }
+
+        letters.Push(c);
+    }
+
+    return new string(letters.Reverse().ToArray());
+}
